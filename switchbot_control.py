f51afd18156191b7b98ef196e0a9d869841a1ee5
Salim Nanji <contact@salimnanji.com>
2023-03-24 14:11:57 -0400

    Read from secrets.yaml (check if in current directory, or parent directory)

--- switchbot_control.py
+++ switchbot_control.py
@@ -20,34 +20,48 @@
 #    switchbot_press_kettle: 'python3 scripts/switchbot_control.py press switchbot_kettle_id'
 #    switchbot_press_espresso_machine: 'python3 scripts/switchbot_control.py press xxxxxxxxxxxx'
 
+import os
 import json
 import sys
 import uuid
 import yaml
 from switchbot import SwitchBot
 
-# Check arguments
+# Check arguments passed in from command line
 args = sys.argv[1:]
-if len(args) == 0 or len(args) > 2:
+if len(args) == 1 and args[0] == 'list': # list requires single arg
+    args.append(None)
+elif len(args) != 2:
     raise Exception('invalid args')
+command, id = args
 
-# Switchbot API authentication
-# Read from secrets.yaml (assuming file is in parent folder)
-# Else remove below lines and hardcode token/secret
-with open('/config/secrets.yaml', 'r') as file:
+# Read from secrets.yaml (check if in current directory, or parent directory)
+secrets_filename = 'secrets.yaml'
+current_directory = os.path.dirname(__file__)
+parent_directory = os.path.dirname(current_directory)
+secrets_current_directory = os.path.join(current_directory, secrets_filename)
+secrets_parent_directory = os.path.join(parent_directory, secrets_filename)
+
+if os.path.exists(secrets_current_directory):
+    secrets_path = secrets_current_directory
+elif os.path.exists(secrets_parent_directory):
+    secrets_path = secrets_parent_directory
+else:
+    raise Exception('missing secrets.yaml')
+
+with open(secrets_path, 'r') as file:
     try:
         secrets = yaml.safe_load(file)
     except yaml.YAMLError as e:
         print(e)
 
+# Switchbot API authentication
 token = secrets.get('switchbot_api_token')
 secret =  secrets.get('switchbot_secret_key')
 switchbot = SwitchBot(token=token, secret=secret, nonce=str(uuid.uuid4()))
 
-# Parse arguments from sys args
-command = args[0]
-id = args[1] if len(args) == 2 else None
-id = secrets.get(id, id) # check if secrets.yaml has the key defined (e.g. switchbot_kettle_id), else use passed-in id
+# Check if secrets.yaml has the key defined (e.g. switchbot_kettle_id), else use passed-in id
+id = secrets.get(id, id)
 
 if command == 'list':
     devices = switchbot.devices()
