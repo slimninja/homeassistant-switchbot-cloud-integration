aa028cd693ea7d4cfcb7da3bcc3c98d44fbda3d2
Salim Nanji <contact@salimnanji.com>
2023-04-06 01:06:07 -0400

    Refactor script in to functions

--- switchbot_cloud.py
+++ switchbot_cloud.py
@@ -34,9 +34,27 @@ import uuid
 import urllib3
 import yaml
 
-# Helper function for sending requests as HomeAssistant doesnt have requests built in
-def request(url, headers, data={}, method='GET'):
+secrets = None
+
+# Helper function for sending requests as HomeAssistant doesnt have the requests module built in
+def request(url, data={}, method='GET'):
+
+    # Switchbot authentication using secrets.yaml
+    token = secrets.get('switchbot_api_token')
+    secret =  secrets.get('switchbot_secret_key')
+    nonce = uuid.uuid4().hex
+
+    t = int(round(time.time() * 1000))
+    string_to_sign = '{}{}{}'.format(token, t, nonce)
+
+    string_to_sign = bytes(string_to_sign, 'utf-8')
+    secret = bytes(secret, 'utf-8')
+
+    sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
+    headers = {"Authorization": (str(token)), "t": (str(t)), "sign": (str(sign, 'utf-8')), "nonce": (str(nonce)), "Content-Type": "application/json; charset=utf8"}
+
     encoded_data = json.dumps(data).encode('utf-8')
+
     http = urllib3.PoolManager()
     r = http.request(method, url, headers=headers, body=encoded_data)
     response = json.loads(r.data.decode('utf-8'))
@@ -50,65 +68,62 @@ def request(url, headers, data={}, method='GET'):
     else:
         return response
 
-# Check arguments passed in from command line
-args = sys.argv[1:]
-if len(args) == 1 and args[0] == 'list': # list requires single arg
-    args.append(None)
-elif len(args) != 2:
-    raise Exception('invalid args')
-command, device_id = args
+# Send data to HomeAssistant as pretty printed sysout
+def output(data):
+    formatted = json.dumps(data)
+    print(formatted)
 
 # Read from secrets.yaml (check if in current directory, or parent directory)
-secrets_filename = 'secrets.yaml'
-current_directory = os.path.dirname(__file__)
-parent_directory = os.path.dirname(current_directory)
-secrets_current_directory = os.path.join(current_directory, secrets_filename)
-secrets_parent_directory = os.path.join(parent_directory, secrets_filename)
+def get_secrets():
+    secrets_filename = 'secrets.yaml'
+    current_directory = os.path.dirname(__file__)
+    parent_directory = os.path.dirname(current_directory)
+    secrets_current_directory = os.path.join(current_directory, secrets_filename)
+    secrets_parent_directory = os.path.join(parent_directory, secrets_filename)
 
-if os.path.exists(secrets_current_directory):
-    secrets_path = secrets_current_directory
-elif os.path.exists(secrets_parent_directory):
-    secrets_path = secrets_parent_directory
-else:
-    raise Exception('missing secrets.yaml')
+    if os.path.exists(secrets_current_directory):
+        secrets_path = secrets_current_directory
+    elif os.path.exists(secrets_parent_directory):
+        secrets_path = secrets_parent_directory
+    else:
+        raise Exception('missing secrets.yaml')
 
-with open(secrets_path, 'r') as file:
-    try:
+    with open(secrets_path, 'r') as file:
+        global secrets
         secrets = yaml.safe_load(file)
-    except yaml.YAMLError as e:
-        print(e)
 
-# Check if secrets.yaml has the key defined (e.g. switchbot_kettle_id), else use passed-in id
-device_id = secrets.get(device_id, device_id)
+def main():
+    # Check arguments passed in from command line
+    args = sys.argv[1:]
+    if len(args) == 1 and args[0] == 'list': # list requires single arg
+        args.append(None)
+    elif len(args) != 2:
+        raise Exception('invalid args')
+    command, device_id = args
 
-# Switchbot authentication
-token = secrets.get('switchbot_api_token')
-secret =  secrets.get('switchbot_secret_key')
-nonce = uuid.uuid4().hex
+    get_secrets()
 
-t = int(round(time.time() * 1000))
-string_to_sign = '{}{}{}'.format(token, t, nonce)
+    if command == 'list':
+        url = f'https://api.switch-bot.com/v1.1/devices'
+        response = request(url)
+        devices = response['deviceList']
+        return output(devices)
 
-string_to_sign = bytes(string_to_sign, 'utf-8')
-secret = bytes(secret, 'utf-8')
+    # Check if secrets.yaml has the key defined (e.g. switchbot_kettle), else use passed-in id
+    device_id = secrets.get(device_id, device_id)
 
-sign = base64.b64encode(hmac.new(secret, msg=string_to_sign, digestmod=hashlib.sha256).digest())
-headers = {"Authorization": (str(token)), "t": (str(t)), "sign": (str(sign, 'utf-8')), "nonce": (str(nonce)), "Content-Type": "application/json; charset=utf8"}
+    if command == 'status':
+        url = f'https://api.switch-bot.com/v1.1/devices/{device_id}/status'
+        response = request(url)
+        return output(response)
 
-# API actions
-if command == 'list':
-    url = f'https://api.switch-bot.com/v1.1/devices'
-    response = request(url, headers)
-    devices = response['deviceList']
-    print(json.dumps(devices))
-elif command == 'status':
-    url = f'https://api.switch-bot.com/v1.1/devices/{device_id}/status'
-    response = request(url, headers)
-    print(json.dumps(response))
-elif command in ['press', 'turnOn', 'turnOff']:
-    url = f'https://api.switch-bot.com/v1.1/devices/{device_id}/commands'
-    data = {"command": command, "commandType": "command"}
-    response = request(url, headers, data, 'POST')
-    print(json.dumps(response))
-else:
-    raise Exception('invalid command')
+    if command in ['press', 'turnOn', 'turnOff']:
+        url = f'https://api.switch-bot.com/v1.1/devices/{device_id}/commands'
+        data = {"command": command, "commandType": "command"}
+        response = request(url, data, 'POST')
+        return output(response)
+
+    raise Exception(f'invalid command specified, "{command}"')
+
+if __name__ == "__main__":
+    main()
